fcfc33afe0fd3888a6daf3667210854f9cac18fc
Sid200026 <siddharthsingharoy@gmail.com>
2020-05-11 18:23:17 +0530

    feat(): Persist redux store and actions, reducers for introduction page

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  testPathIgnorePatterns: [
-    "<rootDir>/frontend/build/",
-    "<rootDir>/node_modules/",
-  ],
-  transformIgnorePatterns: [
-    "<rootDir>/frontend/build/",
-    "<rootDir>/node_modules/",
-  ],
-  roots: ["<rootDir>/frontend/tests/", "<rootDir>/backend/tests"],
+  testPathIgnorePatterns: ['<rootDir>/frontend/build/', '<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['<rootDir>/frontend/build/', '<rootDir>/node_modules/'],
+  roots: ['<rootDir>/frontend/src/tests/', '<rootDir>/backend/tests'],
 };
