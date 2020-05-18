0ce0ce11f2e6c55178c7e2f658b7a7b8d166f3f7
Sid200026 <siddharthsingharoy@gmail.com>
2020-05-19 00:25:24 +0530

    feat(): Added tests for this commit

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -1,5 +1,10 @@
+const path = require('path');
 const request = require('supertest');
 const { app } = require('../app');
+
+const { frontendLogger, logger } = require('../logger/logger');
+frontendLogger.error = jest.fn().mockImplementationOnce(() => {});
+logger.error = jest.fn().mockImplementation(() => {});
 
 describe('Test the route path', () => {
   test('Should send a status code of 200', () => {
@@ -41,4 +46,40 @@ describe('Test whether favicon is served', () => {
         expect(response.type).toBe('image/png');
       });
   });
+});
+
+describe('React Logger is functioning as expected', () => {
+  test('Should return a status code of 201', () => {
+    return request(app)
+      .post('/api/log')
+      .send({ message: 'Some error occured' })
+      .then((response) => {
+        expect(response.statusCode).toBe(201);
+      });
+  });
+});
+
+const imagePath = path.join(__dirname, '../', 'public', 'logo.png');
+
+describe('Upload endpoint should only accept single image', () => {
+  test('Sends status 400', async () =>
+    request(app)
+      .post('/api/upload/single')
+      .attach('image', imagePath)
+      .attach('image', imagePath)
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+      }));
+});
+
+const randomFilePath = path.join(__dirname, '../', 'index.js');
+
+describe('Upload endpoint should accept files with valid image extension', () => {
+  test('Sends status 400', async () =>
+    request(app)
+      .post('/api/upload/single')
+      .attach('image', randomFilePath)
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+      }));
 });
