b891cabec1f9b5d73aef2ac6cb792d4778dc9188
Sid200026 <siddharthsingharoy@gmail.com>
2020-12-01 03:33:22 +0530

    fix(): Fixed user_file.sh issue to execute on linux systems

--- user_files.sh
+++ user_files.sh
@@ -4,13 +4,14 @@ BASE_DIRECTORY='frontend/src/initialState/'
 USER_INTRODUCTION='user.introduction_initial.js'
 USER_ABOUT_ME='user.about_me_initial.js'
 
-FILE_CONTENT="const state = {};\nexport default state;"
+FILE_CONTENT="""const state = {};
+export default state;"""
 
 declare -a USER_FILES=("$USER_INTRODUCTION" "$USER_ABOUT_ME")
 
 function createFile() {
     FILE_PATH=$1
-    echo $FILE_CONTENT >$FILE_PATH
+    echo "$FILE_CONTENT" >> $FILE_PATH
 }
 
 for filename in "${USER_FILES[@]}"; do
